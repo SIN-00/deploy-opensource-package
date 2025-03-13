60c81d337d000f01a797c4bd86e065914e3d7e19
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-03-13 16:12:26 +0900

    py_ad_1_1

--- py_ad_1_1.py
+++ py_ad_1_1.py
@@ -4,8 +4,14 @@ Chapter 1
 Python Advanced(1) - Python Variable Scope
 Keyword - scope, global, nonlocal, locals, globals..
 
+전역변수는 주로 변하지 않는 고정 값에 사용
+지역변수 사용 이유 : 지역변수는 함수 내에 로직 해결에 국한, 소멸주기  : 함수 실행 해제 시
+전역변수를 지역내에서 수정되는 것은 권장X
+
+"""
 """
 
+"""
 # Ex1
 a = 10 # Global variable
 
@@ -63,3 +69,25 @@ def outer():
 in_test = outer()
 
 in_test()
+
+# Ex6
+def func(var):
+    x = 10
+    def printer():
+        print('Ex6 > ', "Printer Func Inner")
+    print('Func Inner', locals()) #지역 전체 출력
+
+func('Hi')
+
+# Ex7
+print('Ex7 > ', globals()) #전역 전체 출력
+globals()['test_variable'] = 100
+print('Ex7 > ', globals())
+
+# Ex8(지역 -> 전역 변수 생성)
+for i in range(1, 10):
+    for k in range(1, 10):
+        globals()['plus_{}_{}'.format(i,k)] = i + k
+
+print(globals())
+print('Ex8 > ', plus_5_5)
